6c4c6601e6efcbb2042e4af211bd8eb98e7990cf
Burak Kayhan <kayhan.burak@outlook.com>
2017-11-25 00:57:55 +0300

    İş ve Sunum(Form)Katmanları oluşturuldu

---
+++ AracServisRandevuSistemiSolution/AracServisRandevuSistemi.Data/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracServisRandevuSistemi.Data
+{
+    public class DataManager
+    {
+        private static string connectionString = "Data Source=DESKTOP-8126EV2;Initial Catalog=AracServisRandevuSistemi;Integrated Security=True";
+
+        private static SqlConnection CreateConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
